6e2532ff9f0bf5a70c0c28e6b471ba9cf77a5087
Jade Allen Cook <jadeallencook@gmail.com>
2017-01-04 20:11:24 -0500

    🐞 Fixed PHP mailer

--- assets/inc/send.php
+++ assets/inc/send.php
@@ -6,14 +6,14 @@
         $errors .= "\n Error: Required Field";
     }
 
-    function sanatize(string) {
+    function sanatize($string) {
         strip_tags($string);
         htmlspecialchars($string, REPLACE_FLAGS, CHARSET);
         return $string;
     }
 
     $name = sanatize($_POST['name']);
-    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL)
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
     $message = sanatize($_POST['message']);
 
     if (!eregi('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$', $email)) {
@@ -22,9 +22,9 @@
 
     if (empty($errors)) {
         $to = $myemail;
-        $email_subject = "A New Message Awaits: {$name}";
-        $email_body = "You have received a new message. Details are given below.\n Name: {$name} \n Email: {$email} \n Message: \n {$message}";
-        $headers = "From: {$email}";
+        $email_subject = "A New Message Awaits: " . $name;
+        $email_body = "You have received a new message. Details are given below.\n Name: " . $name . "\n Email: " . $email . "\n Message: \n" . $message;
+        $headers = "From: " . $email;
         mail($to, $email_subject, $email_body, $headers);
     }
 ?>
