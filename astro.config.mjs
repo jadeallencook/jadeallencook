74b2e76abe3b11ea2b705bd4d03301d4823ed485
Jade Allen Cook <jadeallencook@gmail.com>
2026-07-26 13:03:35 -0700

    fix: set site for astro url fallback

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jadeallencook.com',
   image: {
     domains: ['i.ytimg.com'],
   },
